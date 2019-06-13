592b174d5df4c354efbc2111177c75de6196f979
Kenny Mark <geniounico@outlook.com>
2019-06-13 21:25:29 +0100

    edited redirects and fixed insecure http link

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ import './styles/normalize.css';
 import Route from './components/routes'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(< Route />, document.getElementById('root'));
-
+ReactDOM.render(<Route />, document.getElementById('root'));
 serviceWorker.register();
